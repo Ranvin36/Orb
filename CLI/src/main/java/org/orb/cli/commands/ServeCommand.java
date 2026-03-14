51423ca097a543b713e6b5fcb8ba28a78d635416
Ranvin36 <ranvin.20221587@iit.ac.lk>
2026-03-14 13:51:11 +0530

    Initial server setup & serve command

--- CLI/src/main/java/org/orb/cli/commands/ServeCommand.java
+++ CLI/src/main/java/org/orb/cli/commands/ServeCommand.java
@@ -1,13 +1,45 @@
 package org.orb.cli.commands;
 
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @Command(name = "serve", description = "Serves an ORB")
 public class ServeCommand implements Runnable {
 
+    @Parameters(paramLabel = "REPO_NAME",description = "Name of the repository to index")
+    private String repoName;
+
+
     @Override
     public void run() {
-        System.out.println("Serving a ORB");
+        String home = System.getenv("USERPROFILE");
+        String orbPath = Paths.get(String.valueOf(Path.of(home).resolve("documents")), "orb").toString();
+        Path repoPath = Paths.get(orbPath, repoName);
+        if(Files.notExists(repoPath)){
+            System.err.println("Error: Repository not found in " + orbPath);
+            System.err.println("Hint: Make sure to add the repository using 'orb add <REPO_LINK>' before serving.");
+            return;
+        }
+        else{
+            try (Stream<Path> stream = Files.list(repoPath)) {
+                if (stream.findAny().isEmpty()) {
+                    System.err.println("Error: Repository directory is empty: " + repoPath);
+                    System.err.println("Hint: Make sure the repository was cloned successfully and contains files.");
+                    return;
+                }
+            } catch (IOException e) {
+                System.err.println("Error: Unable to access repository directory: " + repoPath);
+                e.printStackTrace();
+                return;
+            }
+        }
     }
 }
 
